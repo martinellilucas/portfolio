33217563b35553c643414be9ba86b29405475158
Lucas Martinelli <martinellilucas8@gmail.com>
2023-07-05 22:49:41 -0300

    asd

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -10,37 +10,40 @@ const Projects = ({ language }) => {
         <h1 className={style.title}>Proyectos</h1>
       )}
       {language === "eng" ? (
-        <div id="projects" className={style.timeline}>
-          <div data-aos="fade-left" className={style.childImpar}>
-            <h1>Henry's Gym</h1>
-            <p>
-              Web application for a gym with third-party authentication, payment
-              gateway, admin dashboard, subscriptions, user bans and reviews
-            </p>
-            <Link className={style.link} to="https://henry-gym.onrender.com/">
-              Visit
-            </Link>
-          </div>
-          <div data-aos="fade-right" className={style.childPar}>
-            <h1>Healthy Food</h1>
-            <p>
-              Web application for healthy food recipes with filtering, search by
-              name, ordering and recipe creation form.
-            </p>
-            <Link className={style.link} to="https://henry-gym.onrender.com/">
-              Visit
-            </Link>
-          </div>
-          <div data-aos="fade-left" className={style.childImpar}>
-            <h1>Rick & Morty</h1>
-            <p>
-              Web application of the animated series rick and morty with login,
-              search by character id, ordering, filtering and a favorites
-              section.
-            </p>
-            <Link className={style.link} to="https://henry-gym.onrender.com/">
-              Visit
-            </Link>
+        <div className={style.contentGrid}>
+          <div id="projects" className={style.timeline}>
+            <div data-aos="fade-left" className={style.childImpar}>
+              <h1>Henry's Gym</h1>
+              <p>
+                Web application for a gym with third-party authentication,
+                payment gateway, admin dashboard, subscriptions, user bans and
+                reviews
+              </p>
+              <Link className={style.link} to="https://henry-gym.onrender.com/">
+                Visit
+              </Link>
+            </div>
+            <div data-aos="fade-right" className={style.childPar}>
+              <h1>Healthy Food</h1>
+              <p>
+                Web application for healthy food recipes with filtering, search
+                by name, ordering and recipe creation form.
+              </p>
+              <Link className={style.link} to="https://henry-gym.onrender.com/">
+                Visit
+              </Link>
+            </div>
+            <div data-aos="fade-left" className={style.childImpar}>
+              <h1>Rick & Morty</h1>
+              <p>
+                Web application of the animated series rick and morty with
+                login, search by character id, ordering, filtering and a
+                favorites section.
+              </p>
+              <Link className={style.link} to="https://henry-gym.onrender.com/">
+                Visit
+              </Link>
+            </div>
           </div>
         </div>
       ) : (
